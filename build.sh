fa1ca06be9bc11cf95c6144f9889179e5672faab
ph3b3 <phoebeleong8@gmail.com>
2022-02-06 18:44:57 +1100

    moved to c++11

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 rm simpledata &> /dev/null
-if [ $# == 0 ]
-then
-    read -p "Enter a C++ compiler to use: " compiler
-else
-    compiler=$1
-fi
-
-$compiler parser.cpp -o simpledata
+g++ parser.cpp -std=c++11 -o simpledata
 
 if [ -f simpledata ]
 then
